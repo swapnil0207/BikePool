e6c4df9677b43b1cd235f494816ab22a07275e06
Swapnil Jadhav <swapnil.j0207@gmail.com>
2020-08-20 14:01:29 +0530

    To GitHub

--- server/main.js
+++ server/main.js
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import {User} from '../lib/collections';
 Meteor.startup(() => {
   // code to run on server at startup
+  //console.log(process.env);
+//process.env.MONGO_URL='mongodb+srv://swapnil_j0207:Swapnilj2@cluster0.ji79r.mongodb.net/dbname?retryWrites=true&w=majority' ;
 	User.allow({
     'insert': function (_id,name,mobile,pincode,gender,area,email,password) {
       /* user and doc checks ,
